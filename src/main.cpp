2211b24efb7c509cc565e825d59604f8f65ac19a
Himanshu <himanshu.shakya_cs.aiml24@gla.ac.in>
2026-02-20 20:51:05 +0530

    run code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <Arduino.h>
+#include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    dht.begin();
+    Serial.println("DHT11 Sensor Initialized");
 }
 
 void loop() {
-// write your code here
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+    if (isnan(humidity) || isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        delay(2000);
+        return;
+    }
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" *C");
+    delay(2000);
 }
